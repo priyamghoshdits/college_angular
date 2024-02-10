7db8e90b884eeb03a2c1f2e173a0db4cc8ca7416
Avidas1 <133372317+Avidas1@users.noreply.github.com>
2024-02-10 10:59:30 +0530

    updated

---
+++ src/app/components/human-resource/staff-attendance/staff-attendance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-attendance',
+  standalone: true,
+  imports: [],
+  templateUrl: './staff-attendance.component.html',
+  styleUrl: './staff-attendance.component.scss'
+})
+export class StaffAttendanceComponent {
+
+}
