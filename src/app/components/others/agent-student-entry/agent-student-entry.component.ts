97cf3eccc92b92b32e965f9b3ff68b6260d06169
Avidas1 <133372317+Avidas1@users.noreply.github.com>
2024-02-15 11:28:14 +0530

    updated

--- src/app/components/others/agent-student-entry/agent-student-entry.component.ts
+++ src/app/components/others/agent-student-entry/agent-student-entry.component.ts
@@ -4,6 +4,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {NgForOf, NgIf} from "@angular/common";
 import {NgxPaginationModule} from "ngx-pagination";
 import {MemberService} from "../../../services/member.service";
+import {SubjectService} from "../../../services/subject.service";
 
 @Component({
   selector: 'app-agent-student-entry',
@@ -20,23 +21,43 @@ import {MemberService} from "../../../services/member.service";
   styleUrl: './agent-student-entry.component.scss'
 })
 export class AgentStudentEntryComponent {
-    agentForm: FormGroup;
+    studentForm: FormGroup;
     categoryList: any[];
-    constructor(private memberService: MemberService) {
-        this.agentForm = new FormGroup({
+    courseList: any[];
+    semesterList: any[];
+    constructor(private memberService: MemberService, private subjectService: SubjectService) {
+        this.studentForm = new FormGroup({
             id: new FormControl(null),
             first_name: new FormControl(null, [Validators.required]),
+            middle_name: new FormControl(null, [Validators.required]),
             last_name: new FormControl(null, [Validators.required]),
             mobile_no: new FormControl(null, [Validators.required]),
+            admission_status: new FormControl(0),
+            qualification: new FormControl(null, [Validators.required]),
+            current_address: new FormControl(null, [Validators.required]),
+            permanent_address: new FormControl(null, [Validators.required]),
             category_id: new FormControl(null, [Validators.required]),
             email: new FormControl(null, [Validators.required]),
-            commission_percentage: new FormControl(null),
-            commission_flat: new FormControl(null),
+            course_id: new FormControl(null),
+            semester_id: new FormControl(null),
+            agent_id: new FormControl(null),
         });
 
         this.memberService.getCategoryListener().subscribe((response) => {
             this.categoryList = response;
         });
         this.categoryList = this.memberService.getCategoryList();
+
+        this.subjectService.getCourseListener().subscribe((response) => {
+            this.courseList = response;
+        });
+        this.courseList = this.subjectService.getCourses();
+    }
+
+    getSemester(){
+        this.subjectService.getSemesterByCourseId(this.studentForm.value.course_id).subscribe((response) => {
+            // @ts-ignore
+            this.semesterList = response.data;
+        })
     }
 }
