9210d33835be625052d7d744b85c98c009a435b8
Devant Marketing <priyamghosh73@gmail.com>
2024-06-21 12:36:22 +0530

    updated

--- src/app/components/users/user-edit/user-edit.component.ts
+++ src/app/components/users/user-edit/user-edit.component.ts
@@ -50,7 +50,7 @@ export class UserEditComponent implements OnInit {
       admission_date: new FormControl(null),
       image: new FormControl(null),
       mobile_no: new FormControl(null, [Validators.pattern("[0-9 ]{10}")]),
-      emergency_phone_number: new FormControl(null),
+      emergency_phone_number: new FormControl(null,[Validators.pattern("[0-9]{10}")]),
       material_status: new FormControl(null),
       admission_status: new FormControl(null),
       current_address: new FormControl(null),
@@ -61,21 +61,24 @@ export class UserEditComponent implements OnInit {
       email: new FormControl(null),
       course_name: new FormControl(null),
       semester_name: new FormControl(null),
+      current_semester: new FormControl(null),
       agent_id: new FormControl(null),
       father_name: new FormControl(null),
-      father_phone: new FormControl(null),
+      father_phone: new FormControl(null,[Validators.pattern("[0-9]{10}")]),
       father_occupation: new FormControl(null),
       mother_name: new FormControl(null),
-      mother_phone: new FormControl(null),
+      mother_phone: new FormControl(null,[Validators.pattern("[0-9]{10}")]),
       mother_occupation: new FormControl(null),
       guardian_name: new FormControl(null),
-      guardian_phone: new FormControl(null),
+      guardian_phone: new FormControl(null,[Validators.pattern("[0-9]{10}")]),
       guardian_email: new FormControl(null),
       guardian_relation: new FormControl(null),
       guardian_occupation: new FormControl(null),
       guardian_address: new FormControl(null),
       franchise_id: new FormControl(null),
       session_id: new FormControl(null),
+      roll_no: new FormControl(null),
+      registration_no: new FormControl(null),
     });
     this.memberService.getCategoryListener().subscribe((response) => {
       this.categoryList = response;
@@ -87,22 +90,23 @@ export class UserEditComponent implements OnInit {
     this.http.get(this.BASE_API_URL + '/getLoggedInUserData').subscribe((response: any) =>{
       if(response.success == 1){
         this.userDetails = response.data;
-        this.studentCreationForm.patchValue({email: this.userDetails.email
-          , first_name: this.userDetails.first_name
-          , middle_name: this.userDetails.middle_name
-          , last_name: this.userDetails.last_name
-          , course_name: this.userDetails.course_name
-          , semester_name: this.userDetails.current_semester
-          , permanent_address: this.userDetails.permanent_address
-          , gender: this.userDetails.gender
-          , dob: this.userDetails.dob
-          , category_id: this.userDetails.category_id
-          , mobile_no: this.userDetails.mobile_no
-          , blood_group: this.userDetails.blood_group
-          , religion: this.userDetails.religion
-          , material_status: this.userDetails.material_status
-          , identification_no: this.userDetails.identification_no
-        });
+        // this.studentCreationForm.patchValue({email: this.userDetails.email
+        //   , first_name: this.userDetails.first_name
+        //   , middle_name: this.userDetails.middle_name
+        //   , last_name: this.userDetails.last_name
+        //   , course_name: this.userDetails.course_name
+        //   , semester_name: this.userDetails.current_semester
+        //   , permanent_address: this.userDetails.permanent_address
+        //   , gender: this.userDetails.gender
+        //   , dob: this.userDetails.dob
+        //   , category_id: this.userDetails.category_id
+        //   , mobile_no: this.userDetails.mobile_no
+        //   , blood_group: this.userDetails.blood_group
+        //   , religion: this.userDetails.religion
+        //   , material_status: this.userDetails.material_status
+        //   , identification_no: this.userDetails.identification_no
+        // });
+        this.studentCreationForm.patchValue(this.userDetails)
         // @ts-ignore
         // setTimeout(this.showPopup = false,2000);
       }
