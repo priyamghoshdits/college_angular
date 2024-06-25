ef0f312530fff32536b6642174d28f1a4f2f996f
Devant Marketing <priyamghosh73@gmail.com>
2024-06-25 18:29:01 +0530

    updated

---
+++ src/app/components/users/staff-profile/staff-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './staff-profile.component.html',
+  styleUrl: './staff-profile.component.scss'
+})
+export class StaffProfileComponent {
+
+}
