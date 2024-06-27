cb08c7d63748d961a41b45cfdbe487c1fc4b2cef
Devant Marketing <priyamghosh73@gmail.com>
2024-06-27 15:43:31 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   BASE_API_URL: 'https://rgoi.in/college_api/public/api',
-  FILE_URL: 'https://rgoi.in/college_api/public/',
+  FILE_URL: 'https://rgoi.in/college_api/public',
 };
