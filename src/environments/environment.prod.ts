d4ca08534f82a10474f5b163a3db7d90a8f019c9
Krishna-DITS <krishnabasak.dits@gmail.com>
2024-07-02 10:44:32 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_API_URL: 'https://rgoi.in/college_api/public/api',
-  FILE_URL: 'https://rgoi.in/college_api/public',
+  BASE_API_URL: 'https://devanthosting.cloud/demo_erp/api/public/api',
+  FILE_URL: 'https://devanthosting.cloud/demo_erp/api/public',
 };
