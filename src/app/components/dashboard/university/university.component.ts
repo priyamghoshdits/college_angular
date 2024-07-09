4b6c54ca30b37de3e2e317e02e7ad35b96c4a6c4
Krishna-DITS <krishnabasak.dits@gmail.com>
2024-07-09 18:18:18 +0530

    updated

--- src/app/components/dashboard/university/university.component.ts
+++ src/app/components/dashboard/university/university.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
-import {NgbDateStruct, NgbDate, NgbCalendar, NgbDatepickerConfig, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import { NgbDateStruct, NgbDate, NgbCalendar, NgbDatepickerConfig, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { ChartComponent, ChartOptions } from "chart.js";
 import * as chartData from "./../../../shared/data/dashboard/university";
 import * as chartDatas from '../../../shared/data/widgets-chart/chart-widget';
 declare var require: any;
 import * as chart from "../../../shared/data/chart/chartist";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {DatePipe} from "@angular/common";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { DatePipe } from "@angular/common";
 import { barChartSingle, pieChart, multiData, single } from '../../../shared/data/chart/ngx-chart';
 import * as graphoptions from '../../../shared/data/chart/config';
 import { monthlydoughnutData, dailydoughnutData } from '../../../shared/data/widgets-chart/chart-widget';
-import {DashboardService} from "../../../services/dashboard.service";
+import { DashboardService } from "../../../services/dashboard.service";
 
 // var Knob = require('knob') // browserify require
 
@@ -22,9 +22,11 @@ var secondary = localStorage.getItem("secondary_color") || "#1ea6ec";
   templateUrl: "./university.component.html",
   styleUrls: ["./university.component.scss"],
   encapsulation: ViewEncapsulation.None,
-  
+
 })
 export class UniversityComponent implements OnInit {
+  // @ts-ignore
+  public user = JSON.parse(localStorage.getItem('user'));
   public barChartsingle = barChartSingle;
   public multiData = multiData;
   universityFormCalender: FormGroup;
@@ -61,7 +63,18 @@ export class UniversityComponent implements OnInit {
   session_id = null;
   studentChart: any[] = [];
 
-  constructor(private modalService: NgbModal,public datepipe: DatePipe, public dashboardService: DashboardService) {
+  userTypeId: any;
+  liveClassList: any[];
+  studentAttendenceChart: any[] = [];
+  totalAchivement = 0;
+
+
+  constructor(private modalService: NgbModal, public datepipe: DatePipe, public dashboardService: DashboardService) {
+
+    // @ts-ignore
+    // const userType = JSON.parse(localStorage.getItem('user'));
+    this.userTypeId = this.user.user_type_id;
+
     Object.assign(this, { multiData, barChartSingle, pieChart, single });
     Object.assign(this, { monthlydoughnutData, dailydoughnutData })
     this.universityFormCalender = new FormGroup({
@@ -76,39 +89,52 @@ export class UniversityComponent implements OnInit {
     // @ts-ignore
     this.session_id = JSON.parse(localStorage.getItem('session_id'));
 
-    this.dashboardService.getDashboardData().subscribe((response: any) => {
-      this.total_books = response.data.total_books;
-      this.no_of_fees_received = response.data.no_of_fees_received;
-      this.total_fees_received = response.data.total_fees_received;
-      this.total_expense = response.data.total_expense;
-      this.total_student = response.data.total_student;
-      this.total_teacher = response.data.total_teacher;
-      this.total_male_student = response.data.total_male_student;
-      this.total_female_student = response.data.total_female_student;
-      this.noticeList = response.data.notice;
-      this.total_study_material = response.data.studyMaterial;
-      this.total_assignment = response.data.total_assignment;
-      this.total_accountant = response.data.total_accountant;
-      this.dailydoughnutData22 = [
-        {
-          value: this.total_male_student,
-          name: "Male"
+    if (this.userTypeId == 1) {
+      this.dashboardService.getDashboardData().subscribe((response: any) => {
+        this.total_books = response.data.total_books;
+        this.no_of_fees_received = response.data.no_of_fees_received;
+        this.total_fees_received = response.data.total_fees_received;
+        this.total_expense = response.data.total_expense;
+        this.total_student = response.data.total_student;
+        this.total_teacher = response.data.total_teacher;
+        this.total_male_student = response.data.total_male_student;
+        this.total_female_student = response.data.total_female_student;
+        this.noticeList = response.data.notice;
+        this.total_study_material = response.data.studyMaterial;
+        this.total_assignment = response.data.total_assignment;
+        this.total_accountant = response.data.total_accountant;
+        this.dailydoughnutData22 = [
+          {
+            value: this.total_male_student,
+            name: "Male"
 
-        },
-        {
-          value: this.total_female_student,
-          name: "Female"
-        }
-      ];
-      this.studentChart = response.data.student_chart;
-    })
+          },
+          {
+            value: this.total_female_student,
+            name: "Female"
+          }
+        ];
+        this.studentChart = response.data.student_chart;
+      })
+    } else {
+      this.dashboardService.getDashboardDataForStudent().subscribe((response: any) => {
+        this.total_books = response.data.total_books;
+        this.noticeList = response.data.notice;
+        this.liveClassList = response.data.live_class_list;
+        this.total_assignment = response.data.total_assignment;
+        this.total_study_material = response.data.studyMaterial;
+        this.totalAchivement = response.data.total_achivement;
+
+        this.studentAttendenceChart = response.data.student_chart;
+      });
+    }
   }
 
 
 
   // public dailydoughnutData = dailydoughnutData;
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   public chart1 = chartData.chart1;
   public chart2 = chartData.chart2;
@@ -126,12 +152,12 @@ export class UniversityComponent implements OnInit {
 
   public datePick = null;
 
-  openModal(data, content){
-    const new_date_create =new Date(data.year + '-'+data.month +'-'+data.day);
-    const new_date_create4 =this.datepipe.transform(new_date_create, 'yyyy-MM-dd');
-    this.modalService.open(content,{ size: 'xl'});
-    this.universityFormCalender.patchValue({event_from: new_date_create4, event_to: new_date_create4});
+  openModal(data, content) {
+    const new_date_create = new Date(data.year + '-' + data.month + '-' + data.day);
+    const new_date_create4 = this.datepipe.transform(new_date_create, 'yyyy-MM-dd');
+    this.modalService.open(content, { size: 'xl' });
+    this.universityFormCalender.patchValue({ event_from: new_date_create4, event_to: new_date_create4 });
   }
 
-  public onSelect(e) {  }
+  public onSelect(e) { }
 }
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {ErrorService} from "./error.service";
-import {environment} from "../../environments/environment";
-import {Subject} from "rxjs";
-import {catchError, tap} from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { ErrorService } from "./error.service";
+import { environment } from "../../environments/environment";
+import { Subject } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +11,19 @@ import {catchError, tap} from "rxjs/operators";
 export class DashboardService {
   private BASE_API_URL = environment.BASE_API_URL;
 
-  constructor(private  http: HttpClient, private errorService: ErrorService) {
+  constructor(private http: HttpClient, private errorService: ErrorService) {
   }
 
-  getDashboardData(){
+  getDashboardData() {
     return this.http.get(this.BASE_API_URL + '/dashboard')
-        .pipe(catchError(this.errorService.serverError), tap(response => {
-        }));
+      .pipe(catchError(this.errorService.serverError), tap(response => {
+      }));
+  }
+
+  getDashboardDataForStudent() {
+    return this.http.get(this.BASE_API_URL + '/dashboardForStudent')
+      .pipe(catchError(this.errorService.serverError), tap(response => {
+      }));
   }
 
 }
